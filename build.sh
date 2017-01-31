f357259ddf1192f91a883250eca0ba225c613374
Mikel Adot <mikel.adot@beeva.com>
2017-01-31 11:52:14 +0100

    probar ejecucion de jenkins

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 pwd 
 ls -l
+pwd
 echo "------------------------------------"
-echo "Esto se ha ejecutado correctamente--"
+echo "Esto se ha ejecutado correctamente, comprobando si al hacer push se ejecuta en jenkins--"
 echo "------------------------------------"
